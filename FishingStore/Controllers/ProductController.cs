ddebe44467b465d481e898f96c322a65695b2964
brian desktop <cimminob@gmail.com>
2019-11-17 12:21:31 -0500

    Added paging system.

--- FishingStore/Controllers/ProductController.cs
+++ FishingStore/Controllers/ProductController.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using FishingStore.Models;
 using Microsoft.AspNetCore.Mvc;
+using FishingStore.Infrastructure;
+using FishingStore.Models.ViewModels;
 
 namespace FishingStore.Controllers
 {
@@ -17,11 +16,19 @@ namespace FishingStore.Controllers
             repository = repo;
         }
 
-        public ViewResult List(int productPage=1) 
-            => View(repository.Products
-            .OrderBy(p => p.ProductID)
-            .Skip((productPage - 1) * PageSize)
-            .Take(PageSize)        
-            );
+        public ViewResult List(int productPage = 1)
+            => View(new ProductsListViewModel
+            {
+                Products = repository.Products
+                    .OrderBy(p => p.ProductID)
+                    .Skip((productPage - 1) * PageSize)
+                    .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = productPage,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                }
+            });
     }
 }
---
+++ FishingStore/Infrastructure/ProductsListViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FishingStore.Models;
+using FishingStore.Models.ViewModels;
+
+namespace FishingStore.Infrastructure
+{
+    public class ProductsListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+        
+        //public string CurrentCategory { get; set; }
+    }
+}
